4941f7596cbb1915177ca3a4b4cc6619493eaad4
trumanwp <adambowieday3@gmail.com>
2025-04-23 16:04:25 +0100

    Complete word count add : Implement functionality

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        // Taking User Input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your text.");
+        String input = sc.nextLine();
+
+        // Creating Word object to store the text
+        Word text = new Word(input);
+
+        System.out.println(text.countAll());
+
+    }
 }
--- src/Word.java
+++ src/Word.java
@@ -1,2 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Word {
+
+    private String words;
+    private ArrayList<String> wordList;
+
+    public Word(String words) {
+        this.words = words;
+    }
+
+    public ArrayList<String> getWordList() {
+        String[] wordsArray = words.split("\\s+");
+        return new ArrayList<>(Arrays.asList(wordsArray));
+    }
+
+    public Integer countWords(){
+        int count = 0;
+
+        for (String word : getWordList()) {
+            count += 1;
+        }
+
+        return count;
+    }
+
+    public Integer countCharacters(){return words.length();}
+
+    public Integer countParagraphs(){
+        String[] paragraphs = words.split("\\n+");
+        int count = 0;
+
+        for (String paragraph : paragraphs) {
+            if (!paragraph.trim().isEmpty()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Integer countSentences(){
+        String[] sentences = words.split("\\.");
+        int count = 0;
+        for (String sentence : sentences) {
+            if (!sentence.trim().isEmpty()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String countAll(){
+        return countWords() + " words" + "\n"
+                + countCharacters() + " characters" + "\n"
+                + countSentences() + " sentences" + "\n"
+                + countParagraphs() + " paragraphs";
+    }
+
+
 }
